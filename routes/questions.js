16bb7065b267d610467650670c06377212568f44
Dohoon Kim <dohoonk.smg@gmail.com>
2016-03-09 11:28:43 -0800

    Form for questions/new

--- routes/questions.js
+++ routes/questions.js
@@ -1,12 +1,9 @@
-// in node you must call the library eachtime u require it
 var express = require("express"),
-    router = express.Router();
+    router  = express.Router();
 
 router.get("/new", function(request, response, next){
-  response.end("Create New Question");
+  // response.end("Create New Question");
+  response.render("questions/new");
 });
-
-//Specific to node js we need to define this to pass the varaible to other
-//routes
 
 module.exports = router;
