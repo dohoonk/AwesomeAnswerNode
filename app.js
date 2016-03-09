f09f61090aab68c54dfd8d3986c87af597740242
Dohoon Kim <dohoonk.smg@gmail.com>
2016-03-09 12:07:03 -0800

    Mongo db

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var questions = require('./routes/questions');
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/AwesomeAnsersJan2016");
+
 var app = express();
 
 // view engine setup
--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,22 @@
-var express = require("express"),
-    router  = express.Router();
+var express  = require("express"),
+    router   = express.Router(),
+    Question = require("../models/question");
 
 router.get("/new", function(request, response, next){
   // response.end("Create New Question");
   response.render("questions/new");
+});
+
+router.post("/", function(request, response, next){
+  var question = new Question({title: request.body.title,
+                               body:   request.body.body});
+  question.save(function(err, question){
+    if(err) {
+      response.end("Got errors!");
+    } else {
+      response.end(question._id.toString());
+    }
+  });
 });
 
 module.exports = router;
